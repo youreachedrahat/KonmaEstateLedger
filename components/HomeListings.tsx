89450e8c40fdc798d10b09cd0958b414a7adc5a3
Rahat Sayyed <70068895+youreachedrahat@users.noreply.github.com>
2023-12-11 20:59:44 +0530

    Update HomeListings.tsx

--- components/HomeListings.tsx
+++ components/HomeListings.tsx
@@ -328,14 +328,20 @@ export default function HomeListings() {
           </div>
           <div className="w-full flex items-center justify-center">
             <button
-              onClick={handleSubmit}
+              onClick={() => {
+                if (formState.selectedLabel.length < 5) {
+                  handleSubmit();
+                } else {
+                  alert("String length should be 5 characters only");
+                }
+              }}
               className={`text-white   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${
                 isImageLoading ? "bg-gray-300" : "bg-blue-700 hover:bg-blue-800"
               }`}
               disabled={isImageLoading}
             >
               Mint NFT
-            </button>
+            </button>
           </div>
         </div>
       </div>
